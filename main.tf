a586b94b2cf5f8161be655fa7f5c5ad872017280
Júnior Dering <juniordering@hotmail.com>
2024-03-24 10:25:27 -0400

    feat: some changes

--- main.tf
+++ main.tf
@@ -17,7 +17,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-08116b9957a259459"
   instance_type = "t2.micro"
   key_name = "Iac-samsung-pc"
+  # user_data = <<-EOF
+  #               #!/bin/bash
+  #               cd /home/ubuntu
+  #               echo "<h1>Feito pelo Terraform</h1>" > index.html
+  #               nohup busybox httpd -f -p 8080 &
+  #               EOF
   tags = {
-    Name = "Primeira instancia"
+    Name = "Terraform Ansible Python"
   }
 }
